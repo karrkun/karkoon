f29361820ee29c077e5d3c754bd6bc1b8eb752ab
knochenmark <marktodd.hl@gmail.com>
2018-11-21 21:20:59 +0100

    Add some basic app routing with multiple layout support

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
 
-const routes: Routes = [];
+import { SiteLayoutComponent } from './_layout/site-layout/site-layout.component';
+import { HomeComponent } from './home/home.component';
+import { IssueComponent } from './issue/issue.component';
+import { LoginComponent } from './login/login.component';
+import { SettingsComponent } from './settings/settings.component';
+
+const routes: Routes = [
+
+  //Site routes goes here
+  {
+    path: '',
+    component: SiteLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      // { path: 'about', component: AboutComponent },
+      { path: 'issue/:id', component: IssueComponent }
+    ]
+  },
+
+  // App routes goes here here
+  // {
+  //   path: '',
+  //   component: AppLayoutComponent,
+  //   children: [
+  //     { path: 'dashboard', component: DashboardComponent },
+  //     { path: 'profile', component: ProfileComponent }
+  //   ]
+  // },
+
+  //no layout routes
+  { path: 'login', component: LoginComponent },
+  { path: 'settings', component: SettingsComponent },
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
